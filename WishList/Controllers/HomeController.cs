336e086f134164a47b2dc526c337d62074be1b90
Mathieu <mathieu.koumbo@efrei.net>
2020-04-11 01:19:43 +0200

    Correction of module 3

--- WishList/Controllers/HomeController.cs
+++ WishList/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace WishList.Controllers
 {
-    public class HomeControllerController : Controller
+    public class HomeController : Controller
     {
         public IActionResult Index()
         {
